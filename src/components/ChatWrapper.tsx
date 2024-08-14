5250286f609d8c72de6874cd496e47a13e8ed30e
ChandraShekhar <csyadav0513@gmail.com>
2024-08-14 12:54:11 +0530

    feat: Add chat stream API route and ChatWrapper component

--- src/components/ChatWrapper.tsx
+++ src/components/ChatWrapper.tsx
@@ -3,11 +3,27 @@
 import { useChat } from "ai/react";
 
 const ChatWrapper = ({ sessionId }: { sessionId: string }) => {
-  const {} = useChat({
+  const { messages, handleInputChange, handleSubmit, input } = useChat({
     api: "/api/chat-stream",
     body: { sessionId },
   });
 
-  return <div className="replative min-h-full bg-zinc-900">ChatWrapper</div>;
+  return (
+    <div className="replative min-h-full bg-zinc-900 flex divide-y divide-zinc-700 flex-col justify-between gap-2">
+      <div className="flex-1 text-black bg-zinc-800 justify-between flex flex-col ">
+        {JSON.stringify(messages)}
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          className="text-black"
+          value={input}
+          onChange={handleInputChange}
+          type="text"
+        />
+        <button type="submit">send</button>
+      </form>
+    </div>
+  );
 };
 export default ChatWrapper;
